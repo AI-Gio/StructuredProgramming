96830385ae7793142bce7f7ba94834a56ebb4391
AI-Gio <wreckitsnoofs@gmail.com>
2020-02-13 08:53:11 +0100

    Initial Commit

--- Mastermind.py
+++ Mastermind.py
@@ -1,22 +1,80 @@
-def PlayerCode(): #voor pc tegen speler, speler heeft de geheime code
-    print("Choose from colors(red, blue, yellow, green, orange, brown, black, white) for 4 pins")
-    SecretCode = []
-    Colors = ["red", "blue", "yellow", "green", "orange", "brown", "black", "white"]
-    #n = 1
+
+print("Choose from colors(red, blue, yellow, green, orange, brown) for 4 pins")
+
+#===================================================================================================
+#Hier staat de code, voor het player tegen pc, dus pc heeft geheime code en player moet raden
+def Guess():
+    Colors = ["red", "blue", "yellow", "green", "orange", "brown"]
+    Guesses = []
     for i in range(1,5):
-        PinCode = input(f"choose color pin {i} : ")
-        if PinCode not in Colors:
-            print("Sorry this color is not used in Mastermind")
+        PinCode = None
+        while PinCode not in Colors:
+            PinCode = input(f"color pin {i} : ")
+            if PinCode not in Colors:
+                print("Sorry this color is not used in Mastermind")
+        Guesses.append(PinCode)
+    return Guesses
 
-        else:
-            SecretCode.append(PinCode)
-    return SecretCode
-print(PlayerCode())
-
-def PCCode():
+def PCCode(): #voor player tegen pc, pc heeft geheime code
     import random
+    SecretCode = []
     for i in range(1,5):
-        PinCode = random.choice(["red", "blue", "yellow", "green", "orange", "brown", "black", "white"])
-        print(PinCode)
+        PinCode = random.choice(["red", "blue", "yellow", "green", "orange", "brown"])
+        SecretCode.append(PinCode)
+    return SecretCode
+#print(PCCode())
+
+def PCFeedback(SecretCode, Guess):
+    #print(SecretCode, Guess)
+    BlackPins = 0
+    WhitePins = 0
+    SecretCodeTemp = []
+    GuessTemp = []
+    #Black pins
+    for i in range(0,4):
+        if Guess[i] == SecretCode[i]:
+            BlackPins += 1
+        else:
+            SecretCodeTemp.append(SecretCode[i])
+            GuessTemp.append(Guess[i])
+#White pins
+    for i in GuessTemp:
+        for j in SecretCodeTemp:
+            if i == j:
+                SecretCodeTemp.remove(j)
+                WhitePins += 1
+                break
+#Data displayed
+    feedback = "B"*BlackPins + "W"*WhitePins
+    return feedback
+
+def PlayerGuess():
+    SecretCode = PCCode()
+
+    for i in range(1,11):
+        print(f"This is guess {i}\n")
+        guess = Guess()
+        feedback = PCFeedback(SecretCode, guess)
+        if feedback == "BBBB":
+            return f"After {i} guess(es), the secret code is {SecretCode}"
+        elif i == 10:
+            print(f"You lost, the secret code was {SecretCode}")
+        else:
+            print(feedback)
+PlayerGuess()
+#===================================================================================================
+#Hier staat de code voor pc tegen player, dus de player heeft geheime code en pc moet raden
+def PlayerCode(): #voor pc tegen speler, speler heeft de geheime code
+    SecretCode = []
+    Colors = ["red", "blue", "yellow", "green", "orange", "brown"]
+    for i in range(1,5):
+        PinCode = None
+        while PinCode not in Colors:
+            PinCode = input(f"color pin {i} : ")
+            if PinCode not in Colors:
+                print("Sorry this color is not used in Mastermind")
+        SecretCode.append(PinCode)
+    return SecretCode
+
 def MastermindPc(): #Je speelt tegen de pc dus pc geeft feedback en geeft aan of het goed geraden is.
-    peeps = 0
+    x = 0
